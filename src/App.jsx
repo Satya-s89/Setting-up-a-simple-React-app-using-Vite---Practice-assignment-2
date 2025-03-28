5a4b19fe9662a1a8c9db0b984e2e00724dc4bbc5
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-28 13:24:22 +0530

    7

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>KAI HARI SATYA SRIKANTH</h1>
+      <h2>kaki.harisatya.s89@gmail.com</h2>
+    </>
+  )
+}
+
+export default App
